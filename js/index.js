4ca1539daf8e4244002b5b99019efb24ecf41ae4
Manuel Ramirez <manuel.ramirez89@gmail.com>
2017-03-16 18:01:05 -0600

    Cambios 1.1

--- js/index.js
+++ js/index.js
@@ -1,38 +1,56 @@
 window.onload = complete;
 var maincontainter = null;
 var images = null;
+var x = null;
+var y = null;
 function complete()
 {
-	console.log("gola");
 	maincontainter = document.getElementById('contentDrop');
 	//maincontainter.ondragover = allowDrop;
-	contentDrop = document.getElementById('contentPadding');
-	images = contentDrop.getElementsByTagName('div');
+	contentDrop = document.getElementById('content');
+	images = contentDrop.getElementsByClassName ('resize-content');
 	for(var i = 0; i < images.length ; i++)
 	{
 		images[i].ondrag = moveImages;
 		images[i].ondragstart = dragStart;
 		images[i].ondragend = dragEnd;
 	}
+	contentDrop.ondrop = droped;
 }
 function moveImages(e)
 {
 	//console.log(e)
-	console.log(e.pageY+"px");
-	e.target.style.left = e.pageX+"px";
-	e.target.style.top = e.pageY+"px";
+	//y = e.pageX;
+	//x = e.pageY;
 }
-function dragEnd(e)
+function droped(e,ui)
 {
+	console.log("==============================");
 	console.log(e);
-	e.target.style.left = e.pageX+"px";
-	e.target.style.top = e.pageY+"px";
+	console.log(ui);
+	console.log("==============================");
+}
+function dragEnd(e,ui)
+{
+	console.log(ui);
+	console.log(e.position.left);
+	console.log(e.position.top);
+	//console.log(e);
+	e.target.style.left = Math.round(e.pageX + (e.target.offsetWidth / 2)) + "px";
+	e.target.style.top = Math.round(e.pageY + (e.target.offsetHeight / 2)) + "px";
+	console.log(e.pageY);	
+	console.log(e.pageX);
+	console.log(Math.round(e.pageX + (e.target.offsetWidth / 2)) + "px");
+	console.log(Math.round(e.pageY + (e.target.offsetHeight / 2)) + "px");
+	console.log(e.target.offsetWidth);
+	console.log(e.target.offsetHeight);	
 }
 function dragStart()
 {
 	console.log(this);
 }
-function allowDrop()
+function allowDrop(event)
 {
-	console.log("Deja");
+	event.preventDefault();
+	//console.log("Deja");
 }
