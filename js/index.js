ed69991f662a880233dd1a5dbca060b0ced1fef4
Manuel Ramirez <manuel.ramirez89@gmail.com>
2017-03-17 15:26:30 -0600

    Funcional el movimiento del contenedor principal

--- js/index.js
+++ js/index.js
@@ -3,59 +3,59 @@ var maincontainter = null;
 var images = null;
 var x = null;
 var y = null;
+var offset_data;
 function complete()
 {
-	maincontainter = document.getElementById('contentDrop');
-	//maincontainter.ondragover = allowDrop;
+	//maincontainter = document.getElementById('contentDrop');
 	contentDrop = document.getElementById('content');
 	images = contentDrop.getElementsByClassName ('resize-content');
 	for(var i = 0; i < images.length ; i++)
 	{
-		images[i].ondrag = moveImages;
 		images[i].ondragstart = dragStart;
+		images[i].ondrag = dragging;
 		images[i].ondragend = dragEnd;
 	}
-	contentDrop.ondrop = droped;
-	contentDrop.ondragover = allowDrop;
+	content.ondragover = allowDrop;
+	contentDrop.addEventListener('drop',droped,false); 
 }
-function moveImages(e)
+function dragStart(event)
 {
-	//console.log(e)
-	//y = e.pageX;
-	//x = e.pageY;
+	var style = window.getComputedStyle(event.target, null);
+	offset_data = (parseInt(style.getPropertyValue("left"),10) - event.clientX) + ',' + (parseInt(style.getPropertyValue("top"),10) - event.clientY);
+	event.dataTransfer.setData("text/plain",offset_data);
+	event.target.style.cursor = 'move';
 }
-function droped(e,ui)
+function dragging(event)
+{
+	console.log("eco");
+	event.target.style.cursor = 'move'; 
+}
+function droped(event)
+{
+	console.log(event);
+	var offset;
+	try 
+	{
+		console.log(event.dataTransfer.getData("text/plain"));
+		offset = event.dataTransfer.getData("text/plain").split(',');
+	} 
+	catch(e) 
+	{
+		offset = offset_data.split(',');
+	}
+	x = parseInt(offset[0],10);
+	y = parseInt(offset[1],10);
+	event.preventDefault();
+	return false;
+}
+function dragEnd(e)
+{
+	var dm = e.target;
+    dm.style.left = (event.clientX + x) + 'px';
+    dm.style.top = (event.clientY + y) + 'px';
+    e.preventDefault();
+}
+function allowDrop(e)
 {
 	e.preventDefault();
-	console.log("==============================");
-	console.log(e);
-	console.log(this.offsetTop);
-	console.log("==============================");
-}
-function dragEnd(e,ui)
-{
-	console.log(ui);
-	console.log(e.position.left);
-	console.log(e.position.top);
-	//console.log(e);
-	e.target.style.left = Math.round(e.pageX + (e.target.offsetWidth / 2)) + "px";
-	e.target.style.top = Math.round(e.pageY + (e.target.offsetHeight / 2)) + "px";
-	console.log(e.pageY);	
-	console.log(e.pageX);
-	console.log(Math.round(e.pageX + (e.target.offsetWidth / 2)) + "px");
-	console.log(Math.round(e.pageY + (e.target.offsetHeight / 2)) + "px");
-	console.log(e.target.offsetWidth);
-	console.log(e.target.offsetHeight);	
-}
-function dragStart()
-{
-	console.log(this);
-}
-function allowDrop(event,ui)
-{
-	event.preventDefault();
-	console.log(event);
-	console.log(1);
-	console.log(ui);
-	//console.log("Deja");
 }
